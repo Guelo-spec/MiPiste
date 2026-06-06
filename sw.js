e68f3a9e613a37ab39678603d6fc7a69e5ad2791
Gelo Camero <mlcamero131@gmail.com>
2026-06-06 23:48:04 +0800

    Reskin: blue-glass theme + cinematic sabre hero banner & greeting, frosted cards, original AI hero art; cache v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='piste-v6';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
+const CACHE='piste-v7';
+const ASSETS=['./','./index.html','./manifest.json','./hero1.png','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
